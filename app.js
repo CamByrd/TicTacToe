1d423beb8a1aa3d2e6eb10654cc976c47d9b3d4e
CamByrd <50843293+CamByrd@users.noreply.github.com>
2019-05-28 11:03:19 -0500

    TicTacToe

---
+++ app.js
@@ -0,0 +1,47 @@
+
+//declaring variables for the board//
+var TicTacToe;
+const Self= 'X';
+const Start;
+const Ghost_in_Shell = 'O';
+const winconditions= [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2],
+]
+
+
+
+// Code will call columns 
+function start( ) {
+const columns = document.querySelectorAll(".columns");
+}
+
+
+
+const clickaction = () => columns.forEach(function(box){
+
+    box.addEventListener("click", function(3){
+        e.index.html = "O"
+    })
+})
+
+();
+
+const clickaction = () => columns.forEach(function(box){
+    box.addEventListener("click", function(4){
+        e.index.html = "X"
+    })
+})
+
+();
+
+        // Code will switch over from X to O when click function is activated
+
+
+        // 
